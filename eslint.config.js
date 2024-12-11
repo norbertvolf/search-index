fa7b04e5f907e9dab9e9d574243af20e83f913c0
Norbert Volf <norbert.volf@sap.com>
2024-12-11 09:37:36 +0100

    Initial version

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+];
